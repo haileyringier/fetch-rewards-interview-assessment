d59c46216cb475e2d43fec31454c400f4e17cab6
haileyringier <haileyringier@outlook.com>
2020-10-22 15:29:30 -0600

    initial setup of react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+// const dataUrl = 'https://cors-anywhere.herokuapp.com/https://fetch-hiring.s3.amazonaws.com/hiring.json';
+const dataUrl = 'http://localhost:3000/data'
+
+export default function App() {
+
+    const [list, setList] = useState([])
+
+    useEffect(() => {
+      fetch(dataUrl)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        
+    }, [])
+
+  const filterNames = (thingy) => {
+      const filteredList = thingy.filter(item => item.name !== "")
+      console.log(filteredList)
+      setList(filteredList)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch Rewards Interview Assessment</h1>
+      <p>Data is sorted by ListId and whether it contains a name.</p>
+      
     </div>
   );
 }
 
-export default App;
+
